60ed452d8d1f03b97ae36624028d72037828bf14
ganesh-jagu <jaguganesh1683@gmail.com>
2026-06-09 09:36:00 +0530

    Initial commit

---
+++ Servlet-Practice/src/main/java/addnumber/AddServlet.java
@@ -0,0 +1,58 @@
+package addnumber;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class AddServlet extends HttpServlet {
+	
+	// Service method for request  
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException
+	{
+		int a=Integer.parseInt(req.getParameter("num1"));
+		int b=Integer.parseInt(req.getParameter("num2"));
+		
+		int c=a+b;
+		System.out.println("Add of two numbers are:"+c);
+		
+		PrintWriter out=res.getWriter(); 
+		out.println("Addition of two numbers ="+c);
+//		
+//		// Request Dispatcher
+	    req.setAttribute("t", c);
+		RequestDispatcher rd=req.getRequestDispatcher("sqr");
+		rd.forward(req, res);
+		
+	}
+	// dopost method for post request
+	/* public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException
+	{
+		int a=Integer.parseInt(req.getParameter("num1"));
+		int b=Integer.parseInt(req.getParameter("num2"));
+		
+		int c=a+b;
+		System.out.println("Add of two numbers are:"+c);
+		
+		PrintWriter out=res.getWriter(); 
+		out.println("Post Result="+c);
+	} */
+	//doget method for get request
+	 /* public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException
+	{
+		int a=Integer.parseInt(req.getParameter("num1"));
+		int b=Integer.parseInt(req.getParameter("num2"));
+		
+		int c=a+b;
+		System.out.println("Add of two numbers are:"+c);
+		
+		PrintWriter out=res.getWriter(); 
+		out.println("Get Result="+c);
+		
+		
+				} */
+}
---
+++ Servlet-Practice/src/main/java/addnumber/PrintingData.java
@@ -0,0 +1,20 @@
+package addnumber;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class PrintingData extends HttpServlet {
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException
+	{
+		String str=req.getParameter("data1");
+		PrintWriter out=res.getWriter();
+		out.println(str);
+		
+	}
+	
+
+}
